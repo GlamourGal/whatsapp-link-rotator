04f1c53fb3280c7077ec019cda17b0caf11b7ea4
Glamour Gal <147677367+GlamourGal@users.noreply.github.com>
2025-06-13 02:23:27 +0800

    Update get-whatsapp-link.js

--- netlify/functions/get-whatsapp-link.js
+++ netlify/functions/get-whatsapp-link.js
@@ -1,23 +1,8 @@
 exports.handler = async (event) => {
   const links = [
-    "https://wa.me/447715802415",
-    "https://wa.me/13104373610",
-    "https://wa.me/447746809148",
     "https://wa.me/447561657131",
-    "https://wa.me/447955572061", 
-    "https://wa.me/447591102380",
-    "https://wa.me/447563734172",
-    "https://wa.me/447307645423",
-    "https://wa.me/447955572063",
-    "https://wa.me/447955572060",
-    "https://wa.me/447594544338",
-    "https://wa.me/447380121209",
-    "https://wa.me/447955572065",
-    "https://wa.me/447925494381",
-    "https://wa.me/85268835613",
-    "https://wa.me/447756208924",
-    "https://wa.me/447402453051",
-    "https://wa.me/447955572069"
+    "https://wa.me/85263616199",
+    "https://wa.me/447380121209"
   ];
 
   const headers = {
